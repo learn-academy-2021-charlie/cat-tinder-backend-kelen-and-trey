bd433395041116f26378efc6aa08ea5206a48265
Trzy426 <charleswrogersiii@gmail.com>
2021-08-25 14:35:06 -0700

    create backend structure for rails app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cats = [
+  {
+    id: 1,
+    name: "Mittens",
+    age: 5,
+    enjoys: "sunshine and warm spots"
+  },
+  {
+    id: 2,
+    name: "Raisins",
+    age: 4,
+    enjoys: "being queen of the dogs"
+  },
+  {
+    id: 3,
+    name: "Toast",
+    age: 1,
+    enjoys: "getting all the attention"
+  }
+]
+
+cats.each do |attributes|
+   Cat.create attributes
+   puts "creating cat #{attributes}"
+end
